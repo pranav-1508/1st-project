e33a4d74f3d97c811acb7f195bd82ec8065f8c28
pranav-1508 <pranavkadpatil1999@gmail.com>
2023-12-08 04:47:19 +0530

    added result page

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// script.js
 function nextPage() {
     document.getElementById('page1').style.display = 'none';
     document.getElementById('page2').style.display = 'flex';
 }
 
 function previousPage() {
-    document.getElementById('page2').style.display = 'none';
-    document.getElementById('page1').style.display = 'flex';
+    if (document.getElementById('page2').style.display === 'flex') {
+        document.getElementById('page2').style.display = 'none';
+        document.getElementById('page1').style.display = 'flex';
+    } else if (document.getElementById('page3').style.display === 'flex') {
+        document.getElementById('page3').style.display = 'none';
+        document.getElementById('page2').style.display = 'flex';
+    }
 }
+
 
 function submitForm() {
     var runningValue = parseInt(document.getElementById('option1').value, 10) || 0;
@@ -18,5 +23,40 @@ function submitForm() {
 
     document.getElementById('totalPoints').innerText = totalPoints;
     document.getElementById('weightageAverage').innerText = weightageAverage;
-    document.getElementById('result').style.display = 'block';
+
+    // Display the result page
+    document.getElementById('page2').style.display = 'none';
+    document.getElementById('page3').style.display = 'flex';
+
+    // Display the grading information based on total points
+    var grade = getGrade(totalPoints);
+    document.getElementById('grade').innerText = grade;
+
+    var pre = getGradee(totalPoints);
+    document.getElementById('pre').innerText = pre;
+}
+
+function getGrade(totalPoints) {
+    if (totalPoints >= 30) {
+        return 'A ';
+    } else if (totalPoints >= 20) {
+        return 'B ';
+    } else if (totalPoints >= 10) {
+        return 'C ';
+    } else {
+        return 'No Grade';
+    }
+}
+
+
+function getGradee(totalPoints) {
+    if (totalPoints >= 30) {
+        return 'Pass';
+    } else if (totalPoints >= 20) {
+        return 'Pass';
+    } else if (totalPoints >= 10) {
+        return 'Fail';
+    } else {
+        return 'Fail';
+    }
 }
